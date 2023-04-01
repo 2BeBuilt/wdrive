828aa16c51c4a6c1efc3256f7f6ed678a17fd902
Andrey Gruzdev <agruzdev1@gmail.com>
2023-04-02 00:46:58 +0200

    big update

---
+++ next/components/Navigation/Footer.js
@@ -0,0 +1,49 @@
+import FlexCenter from '../Common/FlexCenter'
+import { Button, Container, Text, Stack, Image, Flex } from '@chakra-ui/react'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <Flex
+      bg="gray.900"
+      color="gray.200"
+      as="footer"
+      position="fixed"
+      top="91vh"
+      width="100vw"
+      zIndex={10}
+    >
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}
+      >
+        <Link href={'https://axelar.network/'} target="_blank">
+          <FlexCenter>
+            <Text>Powered by Axelar</Text>
+            <Image
+              ml={2}
+              rounded="full"
+              width={8}
+              height={8}
+              src={'/axelar.png'}
+            />
+          </FlexCenter>
+        </Link>
+        <Stack direction={'row'} spacing={6}>
+          <Button
+            onClick={() => {
+              window.open('https://github.com/Canvinus/warp-drive', '_blank')
+            }}
+          >
+            Github
+          </Button>
+        </Stack>
+      </Container>
+    </Flex>
+  )
+}
