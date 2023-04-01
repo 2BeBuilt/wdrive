9d6c0865d77d44c8c32630d0b9add9049439b07a
Andrey Gruzdev <agruzdev1@gmail.com>
2023-04-01 16:55:13 +0200

    add nft view

---
+++ next/components/NFT/nftView.js
@@ -0,0 +1,78 @@
+import {
+  Spinner,
+  Box,
+  Center,
+  useColorModeValue,
+  Text,
+  Stack,
+  Image,
+  Flex,
+  SimpleGrid,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+export default function NftView({ tokenId, image }) {
+  return (
+    <Center py={12}>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'purple.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        zIndex={1}
+      >
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height={'230px'}
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${image})`,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}
+        >
+          {image ? (
+            <Image
+              marginTop={16}
+              rounded={'lg'}
+              height={230}
+              width={282}
+              objectFit={'cover'}
+              src={image}
+            />
+          ) : (
+            <Flex direction="column" align="center" justify="center">
+              <Spinner size="xl" />
+            </Flex>
+          )}
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            {`NFT #${tokenId}`}
+          </Text>
+          <SimpleGrid columns={3} spacing={2}>
+            {/* functions */}
+          </SimpleGrid>
+          <Text>Nft name</Text>
+        </Stack>
+      </Box>
+    </Center>
+  )
+}
--- next/pages/space-port/index.js
+++ next/pages/space-port/index.js
@@ -1,7 +1,18 @@
-export default function SpacePort() {
+import NftView from '@/components/NFT/nftView'
+
+import { SimpleGrid } from '@chakra-ui/react'
+
+export default function SpacePort({ address }) {
+  const nfts = []
+  for (let i = 0; i < 10; i++) {
+    nfts.push({ key: i })
+  }
+
   return (
-    <div>
-      <h1>Space Port</h1>
-    </div>
+    <SimpleGrid columns={5} spacing={6} marginLeft={4} marginRight={4}>
+      {nfts.map((nft) => (
+        <NftView key={nft.key} tokenId={nft.key} image={'/logo.png'} />
+      ))}
+    </SimpleGrid>
   )
 }
