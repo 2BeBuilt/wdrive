06d7bb953c4f99ea135de0ffff5b7c6072dfa6a7
Andrey Gruzdev <agruzdev1@gmail.com>
2023-04-01 23:18:30 +0200

    ad minting

---
+++ next/pages/mint/index.js
@@ -0,0 +1,80 @@
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from 'wagmi'
+import { Tooltip, Image } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/react'
+import { useNetwork, useAccount } from 'wagmi'
+import { useEffect, useState } from 'react'
+import abi from '@/utils/constants/abi.json'
+import chains from '@/utils/constants/chains.json'
+import FlexCenter from '@/components/Common/FlexCenter'
+
+export default function Mint() {
+  const [shipLogo, setShipLogo] = useState(null)
+  const [contract, setContract] = useState(null)
+  const { address, isConnected } = useAccount()
+  const { chain } = useNetwork()
+  const toast = useToast()
+
+  useEffect(() => {
+    const chainFound = chains.filter((c) => c.name === chain.network)[0]
+    setShipLogo(chainFound.ship)
+    setContract(chainFound.address)
+  }, [chain])
+  console.log(contract)
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: contract,
+    abi: abi,
+    functionName: 'safeMint',
+    args: [address],
+  })
+  const { data, write, error } = useContractWrite(config)
+  const { isLoading, isSuccess, isError } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  if (error) {
+    toast({
+      title: 'Error',
+      description: 'Something went wrong',
+      position: 'top',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+    })
+  }
+
+  if (data) {
+    toast({
+      title: 'Success',
+      description: 'Ship minted',
+      position: 'top',
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+    })
+  }
+
+  return (
+    <FlexCenter>
+      {isConnected && (
+        <Tooltip label="Build Ship" placement="bottom">
+          <Image
+            cursor="pointer"
+            onClick={write}
+            rounded="lg"
+            height={300}
+            width={300}
+            src={shipLogo}
+          />
+        </Tooltip>
+      )}
+    </FlexCenter>
+  )
+}
