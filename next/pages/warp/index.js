7a898d45482fca38de48b2089a39cb834543c753
Andrey Gruzdev <agruzdev1@gmail.com>
2023-04-01 15:57:26 +0200

    update logic

--- next/pages/warp/index.js
+++ next/pages/warp/index.js
@@ -3,6 +3,7 @@ import {
   useContractWrite,
   useWaitForTransaction,
 } from 'wagmi'
+import { useSigner } from 'wagmi'
 import { ethers } from 'ethers'
 import { useState } from 'react'
 import { calculateBridgeFee } from '@/utils/bridgeFee'
@@ -17,42 +18,34 @@ const contractAddress = source.contract.address
 const message = 'test123'
 
 export default function Warp() {
+  const { data: signer } = useSigner()
   const [totalFee, setTotal] = useState(0)
   const [remoteFee, setRemote] = useState(0)
-  const {
-    config,
-    error: prepareError,
-    isError: isPrepareError,
-  } = usePrepareContractWrite({
-    address: contractAddress,
-    abi: abi,
-    functionName: 'sendContractCall',
-    args: [
-      destination.name,
-      destination.receiver.address,
-      ethers.utils.defaultAbiCoder.encode(['string'], [message]),
-      remoteFee,
-    ],
-    overrides: {
-      gasLimit: 1000000,
-      value: 1000000,
-    },
-  })
-  const { data, write, error } = useContractWrite(config)
-  const { isLoading, isSuccess, isError } = useWaitForTransaction({
-    hash: data?.hash,
-  })
 
-  {
-    error && console.log(error)
-  }
-  {
-    data && console.log(data)
-  }
+  const call = async () => {
+    const contract = new ethers.Contract(contractAddress, abi, signer)
 
+    const feeRemote = await calculateBridgeFee(source, destination)
+    const feeSource = await calculateBridgeFee(source, source)
+    const payload = ethers.utils.defaultAbiCoder.encode(['string'], [message])
+
+    const tx = await contract
+      .sendContractCall(
+        destination.name,
+        destination.receiver.address,
+        payload,
+        feeRemote,
+        {
+          value: ethers.BigNumber.from(feeRemote).add(feeSource),
+        }
+      )
+      .then(async (tx) => {
+        console.log(tx)
+        tx.wait()
+      })
+  }
   const handleClick = async () => {
     const feeRemote = await calculateBridgeFee(source, destination)
-    setRemote(feeRemote)
     const feeSource = await calculateBridgeFee(source, source)
     const total = ethers.BigNumber.from(feeRemote).add(feeSource)
     setTotal(total)
@@ -60,7 +53,7 @@ export default function Warp() {
 
   return (
     <div>
-      <Button onClick={handleClick}>Calculate Fee</Button>
+      <Button onClick={call}>Calculate Fee</Button>
       {totalFee && remoteFee && <Button onClick={write}>Send Tx</Button>}
     </div>
   )
